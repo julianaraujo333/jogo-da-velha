d7574948a3dccbfee45ae9ffb6eec476eb68517e
Juliana <juliana.silva@prod.ag>
2023-03-31 20:10:51 -0300

    creating hashtag component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import React from 'react';
+import './App.css';
 import Hashtag from './components/Hashtag';
 import Menu from './components/Header';
 
-function App() {
-  return (
-    <>
-      <Menu/>
-      <Hashtag/>
-    </>
-  );
-}
+const App = () =>(
+  <main className='app'>
+    <Menu/>
+    <Hashtag/>
+  </main>
+);
 
 export default App;
