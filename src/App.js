88100a55ce02eafbe459c103fb1cbe66b262fba5
Juliana <juliana.silva@prod.ag>
2023-03-31 16:36:54 -0300

    adding object card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Card from './objects/Card';
 import Menu from './components/Header';
 
 function App() {
   return (
     <>
       <Menu/>
+      <Card/>
     </>
   );
 }
