ee8e7edf4a1e4d072e37312d60c6a01a28b0bdb1
Juliana <juliana.silva@prod.ag>
2023-03-31 20:37:37 -0300

    adding object label

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Hashtag from './components/Hashtag';
 import Menu from './components/Header';
+import Label from './objects/Label';
 
 const App = () =>(
   <main className='app'>
     <Menu/>
     <Hashtag/>
+    <Label content="Mostrar eventos"/>
   </main>
 );
 
